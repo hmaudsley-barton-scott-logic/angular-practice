70c6b642295782ac1b0da792c1d960417025b1ba
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2026-02-17 14:59:18 +0000

    Add component to task

--- my-angular-app/src/app/components/task/task.ts
+++ my-angular-app/src/app/components/task/task.ts
@@ -3,20 +3,24 @@ import { TaskModel } from '../../types/TaskModel';
 import { DatePipe, AsyncPipe } from '@angular/common';
 import { TaskService } from '../../services/task.service';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { map, switchMap, startWith } from 'rxjs/operators';
+import { TaskStatusUpdate } from '../task-status-update/task-status-update';
 
 @Component({
   selector: 'app-task',
-  imports: [DatePipe, AsyncPipe],
+  imports: [DatePipe, AsyncPipe, TaskStatusUpdate],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
 export class Task {
   private readonly taskService = inject(TaskService);
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly refreshTrigger$ = new Subject<void>();
 
-  task$: Observable<TaskModel | null> = this.activatedRoute.paramMap.pipe(
+  task$: Observable<TaskModel | null> = this.refreshTrigger$.pipe(
+    startWith(undefined),
+    switchMap(() => this.activatedRoute.paramMap),
     map((params) => params.get('taskId')),
     switchMap((id) => {
       if (id === null) {
@@ -25,4 +29,8 @@ export class Task {
       return this.taskService.getTask(id);
     }),
   );
+
+  onStatusUpdated() {
+    this.refreshTrigger$.next();
+  }
 }
