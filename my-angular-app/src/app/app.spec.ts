fb275a709f8a02f65c73d97c9316a606caf7925c
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2026-02-19 15:48:43 +0000

    Unused import

--- my-angular-app/src/app/app.spec.ts
+++ my-angular-app/src/app/app.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
